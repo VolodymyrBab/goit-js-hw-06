fcab8cdb415d7f0123a365dba4d3f8f935ce855f
VolodymyrBab <volbv@yahoo.com>
2023-03-26 22:00:41 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createIngredientsItem = ingredients.map(ingredient => {
+  // создаем лишку
+  const ingredientsItemRef = document.createElement('li');
+  // добавляем текст в ли
+  ingredientsItemRef.textContent = ingredient;
+  // добавляем класс для ли
+  ingredientsItemRef.classList.add('item');
+  return ingredientsItemRef;
+  }
+);
+
+// вставляем лишки в список
+const ingredientsRef = document.querySelector('#ingredients');
+
+ingredientsRef.append(...createIngredientsItem);
